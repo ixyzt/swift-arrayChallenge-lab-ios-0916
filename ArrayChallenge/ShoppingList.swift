0fa8b9e80d56d3db29e82eeae1b96e8c472b8ff6
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-09-27 13:57:52 -0400

    Complete

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -8,12 +8,23 @@
 
 
 class ShoppingList {
+    // Our blank piece of paper
+    var shoppingList: [String] = []
     
+    // Quantity of items
+    var quantityOfItems: [String] = ["6", "4", "12", "4"]
+    
+    // Name of items
+    var nameOfItems: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
         
        
-        // Implement this function
+        var newShoppingList: [String] = []
+        for (index, value) in amounts.enumerated() {
+            newShoppingList.append("\(index + 1). \(items[index])(\(value))")
+        }
+        return newShoppingList
         
         
         
